6669597d2bf084c3d2908fc5de9f65332844560b
Wojciech Klyszejko <wojkly@gmail.com>
2020-10-25 12:17:28 +0100

    finish lab2'

--- src/main/agh/cs/lab2/World.java
+++ src/main/agh/cs/lab2/World.java
@@ -2,11 +2,10 @@ package agh.cs.lab2;
 
 public class World {
     public static void main(String[] args) {
-        MapDirection xd = MapDirection.NORTH;
-        System.out.println(xd.toString());
-        System.out.println(xd.next ());
-        System.out.println(xd.previous());
-        Vector2d a = xd.toUnitVector();
-        System.out.println( a.toString() );
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 }
